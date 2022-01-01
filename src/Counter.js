e0b844887895ae060ae7448b53699589bf9429ff
srinivasaleti <asrinivasarao@seek.com.au>
2022-01-01 10:35:21 +0530

    useEffect Deps Example

--- src/Counter.js
+++ src/Counter.js
@@ -2,17 +2,23 @@ import { useState, useEffect } from "react"
 
 export function Counter() {
     const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
 
-    const onClickListener = () => {
+    const onClickListenerX = () => {
         setX(x + 1);
+    }
+    const onClickListenerY = () => {
+        setY(y + 1);
     }
 
     useEffect(function(){
-        document.title = "You Clicked " + x + " times";
-    });
+        document.title = "You Clicked " + y + " times";
+    }, [y]);
 
     return <div>
-        <h1>Count : {x}</h1>
-        <button onClick={onClickListener}>Increment</button>
+        <h1>X: Count : {x}</h1>
+        <h1>Y: Count : {y}</h1>
+        <button onClick={onClickListenerX}>Increment X</button>
+        <button onClick={onClickListenerY}>Increment Y</button>
     </div>
 }
